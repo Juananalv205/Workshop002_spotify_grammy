8c0ec2157569217fae94a6d40a1fdf15140f2f7c
juananalv205 <juan_and.lopez@uao.edu.co>
2024-09-28 12:04:35 -0500

    feat(000_data_load.ipynb): create seeds and table schemes

---
+++ sql/schema_grammy_staging.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "grammy_staging" (
+    "year" INTEGER,
+    "title" TEXT,
+    "published_at" TEXT,
+    "updated_at" TEXT,
+    "category" TEXT,
+    "nominee" TEXT,
+    "artist" TEXT,
+    "workers" TEXT,
+    "img" TEXT,
+    "winner" BOOLEAN
+);
